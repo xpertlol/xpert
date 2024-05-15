0cc35dc9aca82aeebe44fe103de10aab2ba0a849
xpert <90223556+xpertlol@users.noreply.github.com>
2024-05-15 13:16:06 -0600

    music test

---
+++ script.js
@@ -0,0 +1,14 @@
+// Get the audio element
+const musicPlayer = document.getElementById('musicPlayer');
+
+// Play the audio
+function playMusic() {
+    musicPlayer.play();
+}
+
+// Pause the audio
+function pauseMusic() {
+    musicPlayer.pause();
+}
+
+// You can also add more functionalities like volume control, etc.
