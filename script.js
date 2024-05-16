0b3ed3a050d7e5307b4995dff3068a0c46334502
xpert <90223556+xpertlol@users.noreply.github.com>
2024-05-15 18:29:43 -0600

    = true

--- script.js
+++
@@ -1,8 +0,0 @@
-const audio = document.getElementById('myAudio');
-const volumeButton = document.querySelector('.volume-button');
-
-let isMuted = false;
-
-volumeButton.addEventListener('click', () => {
-    isMuted = !isMuted;
-    audio.muted =
